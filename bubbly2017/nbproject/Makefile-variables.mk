cd9d8c29d125301ae95bfe8e895a988652df5e22
Philicoe <philip.coetzee@gmail.com>
2018-05-28 14:45:27 +0200

    Add files via upload

---
+++ bubbly2017/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Release configuration
+CND_PLATFORM_Release=Gnueabi-Windows
+CND_ARTIFACT_DIR_Release=dist/V5S/App
+CND_ARTIFACT_NAME_Release=bubbly2017
+CND_ARTIFACT_PATH_Release=dist/V5S/App/bubbly2017
+CND_PACKAGE_DIR_Release=dist/Release/Gnueabi-Windows/package
+CND_PACKAGE_NAME_Release=bubbly2017.tar
+CND_PACKAGE_PATH_Release=dist/Release/Gnueabi-Windows/package/bubbly2017.tar
+# Debug configuration
+CND_PLATFORM_Debug=Gnueabi-Windows
+CND_ARTIFACT_DIR_Debug=dist/V5S/Debug
+CND_ARTIFACT_NAME_Debug=bubbly2017
+CND_ARTIFACT_PATH_Debug=dist/V5S/Debug/bubbly2017
+CND_PACKAGE_DIR_Debug=dist/Debug/Gnueabi-Windows/package
+CND_PACKAGE_NAME_Debug=bubbly2017.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Gnueabi-Windows/package/bubbly2017.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
